2332d9f1b63023a8e909f46a31ed0f37abf87355
Ashita Thakur Personal <ashitathakur2807@gmail.com>
2026-07-07 23:59:00 +0530

    added assignment q1

---
+++ Java-Basics/Module-01/Lectuire06-If-Else/Assignment/Checkevenodd.java
@@ -0,0 +1,19 @@
+/*
+Problem Description
+
+Write a program to input an integer from user and print 1 if it is odd otherwise print 0.
+*/
+
+import java.util.Scanner;
+
+public class Checkevenodd {
+     public static void main(String[] args) {
+      Scanner sc= new Scanner(System.in);
+      int n=sc.nextInt();
+      if(n%2==0){
+          System.out.println(0);
+      }else{
+          System.out.println(1);
+      }
+    }
+}
